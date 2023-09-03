e0b17b58c9c1cdd7b8f09003c259e66385ced0db
Alisson Costa <alissonrcosta94@gmail.com>
2023-09-03 18:03:41 -0300

    vercel file 3

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -9,6 +9,7 @@ export const sequelize = new Sequelize(
     process.env.MYSQL_PASSWORD as string,
     {
         dialect: 'mysql',
+        host: process.env.MYSQL_HOST as string,
         port: parseInt(process.env.MYSQL_PORT as string)
     }
 );
